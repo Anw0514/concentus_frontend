82aac3419805c283b65398a39800926c42e1ec21
github username <github email address>
2019-04-06 14:06:00 -0400

    nav links to discover, mypages, create

--- src/Navigation/NavBar.js
+++ src/Navigation/NavBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Dropdown, Icon, Menu, Segment } from "semantic-ui-react";
+import { Dropdown, Icon, Menu } from "semantic-ui-react";
+import { Link, BrowserRouter as Router, Redirect, NavLink } from 'react-router-dom'
 
 class NavBar extends Component {
     render() {
@@ -8,14 +9,20 @@ class NavBar extends Component {
             <Menu inverted pointing secondary>
             <Menu.Item name="Concentus" active={false}><h1>Concentus</h1></Menu.Item>
               <Menu.Menu position="right">
-                <Menu.Item name="discover" active={false} onclick={() => {}}><Icon name="list ul" size="big"/></Menu.Item>
+                  <Menu.Item name="discover" active={false} onclick={() => {}}>
+                  <NavLink to='/discover'><Icon name="list ul" size="big"/></NavLink>
+                  </Menu.Item>
                 <Dropdown item icon="big user circle" simple >
                   <Dropdown.Menu>
                   <Dropdown.Item active={false} onClick={() => {}}>
+                    <NavLink to='/mypages'>
                     <Icon name="list alternate outline" />My Pages
+                    </NavLink>
                   </Dropdown.Item>
                   <Dropdown.Item active={true} onClick={() => {}}>
-                    <Icon name="compose" />New Page
+                      <NavLink to='/create'>
+                        <Icon name="compose" />New Page
+                    </NavLink>
                   </Dropdown.Item>
                   <Dropdown.Item active={false} onClick={() => { }}>
                     <Icon name="settings" />Settings
