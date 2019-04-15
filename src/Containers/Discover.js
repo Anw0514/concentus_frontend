28fbca20112a9f32e43a83c00ca0848aaae433eb
github username <github email address>
2019-04-15 16:29:14 -0400

    can search by genre or name

--- src/Containers/Discover.js
+++ src/Containers/Discover.js
@@ -8,17 +8,59 @@ class Discover extends Component {
         super()
         this.state = {
             searchTerm: '',
-            pageType: ''
+            pages: [],
+            typePages: [],
+            results: [],
+            genre: false
         }
+    }
+
+    componentDidMount() {
+        if (this.props.pages) {
+            this.setState({
+                pages: this.props.pages,
+                results: this.props.pages,
+                typePages: this.props.pages
+            })
+        }
+    }
+
+    handleSearchTypeChange = () => {
+        this.setState({
+            genre: !this.state.genre
+        })
+    }
+
+    handleSearchChange = (term) => {
+        let pages = this.state.typePages
+        let results = []
+        this.state.genre ?
+        results = pages.filter(page => (
+            !!page.genres.find(genre => genre.value.toLowerCase().includes(term))
+        )) : (
+        results = pages.filter(page => page.name.toLowerCase().includes(term))
+        );
+        this.setState({ results, searchTerm: term })
+    }
+    
+    handlePageTypeChange = (type) => {
+        let typePages = this.state.pages.filter(page => (
+            page.model === type
+        ))
+        this.setState({ typePages }, this.handleSearchChange(this.state.searchTerm))
     }
 
     render() {
         return (
             <div className='pageDiv'>
-                <strong>Discover</strong>
-                <SearchBar searchTerm={this.state.searchTerm} type={this.state.pageType} />
+                <SearchBar 
+                    searchTerm={this.state.searchTerm}
+                    genre={this.state.genre}
+                    changeType={this.handleSearchTypeChange} 
+                    changeSearch={this.handleSearchChange}
+                />
                 <br />
-                <PageIndex pages={this.props.pages} discover={true} />
+                <PageIndex pages={this.state.results} discover={true} />
             </div>
         )
     }
--- src/Navigation/SearchBar.js
+++ src/Navigation/SearchBar.js
@@ -1,10 +1,29 @@
-import React, {Component} from 'react'
+import React, {Component, Fragment} from 'react'
+import { Container, Checkbox, Input } from 'semantic-ui-react';
 
 class SearchBar extends Component {
     render() {
-        return(
-            <div>SearchBar</div>
-        )
+        return (
+          <Fragment>
+            <Container fluid className="searchBar">
+              <strong>Search By: &nbsp;&nbsp;</strong>
+              Name&nbsp;&nbsp;
+              <Checkbox
+                slider
+                onClick={this.props.changeType}
+                checked={this.props.genre}
+              />
+              &nbsp;&nbsp; Genre &nbsp;&nbsp;
+              <Input
+                size="small"
+                icon="search"
+                placeholder="Search..."
+                onChange={(e, {value}) => this.props.changeSearch(value.toLowerCase())}
+                value={this.props.searchTerm}
+              />
+            </Container>
+          </Fragment>
+        );
     }
 }
 
